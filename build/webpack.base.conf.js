b900f42d82581d688657562b2ef57e48e008c820
lincenying <lincenying@qq.com>
2016-09-08 21:42:03 +0800

    happypack

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,9 +1,9 @@
-/* global require, module, __dirname */
-
 var path = require('path')
 var webpack = require('webpack')
 var autoprefixer = require('autoprefixer')
 var browserslist = require('browserslist')
+var HappyPack = require('happypack')
+
 var config = require('../config')
 var utils = require('./utils')
 var entris = require('./entris')
@@ -36,12 +36,14 @@ baseWebpackConfig = {
     module: {
         loaders: [{
             test: /\.vue$/,
-            loader: 'vue'
+            loader: 'vue',
+            happy: { id: 'vue' }
         }, {
             test: /\.js$/,
             loader: 'babel',
             include: projectRoot,
-            exclude: /node_modules/
+            exclude: /node_modules/,
+            happy: { id: 'js' }
         }, {
             test: /\.json$/,
             loader: 'json'
@@ -71,6 +73,8 @@ baseWebpackConfig = {
         autoprefixer({ browsers: browserslist('last 2 version, > 0.1%')})
     ],
     plugins: [
+        new HappyPack({ id: 'vue', threads: 4 }),
+        new HappyPack({ id: 'js', threads: 4 }),
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
