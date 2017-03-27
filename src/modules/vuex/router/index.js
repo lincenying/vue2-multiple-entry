82e8a8bf1ff0e3ddf2620378a0480774fe64675b
lincenying <lincenying@qq.com>
2017-03-27 11:17:05 +0800

    Update index.js

--- src/modules/vuex/router/index.js
+++ src/modules/vuex/router/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-const index = () => System.import('../pages/index.vue')
-const view = () => System.import('../pages/view.vue')
+// 懒加载
+// const index = resolve => require(['../pages/index.vue'], resolve)
+// const view = resolve => require(['../pages/view.vue'], resolve)
+// 懒加载 - 按组
+const index = r => require.ensure([], () => r(require('../pages/index.vue')), 'group-index')
+const view = r => require.ensure([], () => r(require('../pages/view.vue')), 'group-view')
 
 Vue.use(VueRouter)
 
