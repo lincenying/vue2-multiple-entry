91aedda5f565e8d5ad29e6802ba5d341b0028ae2
LinCenYing <lincenying@qq.com>
2017-11-26 13:31:14 +0800

    Update nav-component.vue

--- src/components/nav-component.vue
+++ src/components/nav-component.vue
@@ -1,19 +1,19 @@
 <template>
     <ul class="menuOpen">
         <li :class="{current: current === 1}">
-            <a href="./index"><i></i>这是首页</a>
+            <a href="/index"><i></i>这是首页</a>
         </li>
         <li :class="{current: current === 2}">
-            <a href="./module2"><i></i>这是模块2</a>
+            <a href="/module2"><i></i>这是模块2</a>
         </li>
         <li :class="{current: current === 3}">
-            <a href="./module3"><i></i>这是模块3</a>
+            <a href="/module3"><i></i>这是模块3</a>
         </li>
         <li :class="{current: current === 4}">
-            <a href="./router"><i></i>这是带路由的模块</a>
+            <a href="/router"><i></i>这是带路由的模块</a>
         </li>
         <li :class="{current: current === 5}">
-            <a href="./vuex"><i></i>这是带Vuex的模块</a>
+            <a href="/vuex"><i></i>这是带Vuex的模块</a>
         </li>
     </ul>
 </template>
