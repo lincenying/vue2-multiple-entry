fd626d19f5b8684ec3dfe8063882148b584a78fe
林岑影 <lincenying@qq.com>
2018-05-22 11:42:59 +0800

    热更新

--- build/dev-server.js
+++ build/dev-server.js
@@ -27,14 +27,14 @@ const devMiddleware = require('webpack-dev-middleware')(compiler, {
 
 const hotMiddleware = require('webpack-hot-middleware')(compiler)
 // force page reload when html-webpack-plugin template changes
-compiler.plugin('compilation', function(compilation) {
-    compilation.plugin('html-webpack-plugin-after-emit', function(data, cb) {
-        hotMiddleware.publish({
-            action: 'reload'
-        })
-        cb()
-    })
-})
+// compiler.plugin('compilation', function(compilation) {
+//     compilation.plugin('html-webpack-plugin-after-emit', function(data, cb) {
+//         hotMiddleware.publish({
+//             action: 'reload'
+//         })
+//         cb()
+//     })
+// })
 
 // proxy api requests
 Object.keys(proxyTable).forEach(function(context) {
