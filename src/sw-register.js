93f10d62a86701945963b64d7362da761507f4da
林岑影 <lincenying@qq.com>
2018-06-03 23:37:53 +0800

    up

--- src/sw-register.js
+++ src/sw-register.js
@@ -5,7 +5,7 @@
  */
 
 // 注册的地址为 sw-precache-webpack-pulgin 生成的 service-worker.js 或者自己手动维护的 service worker 文件
-if (navigator.serviceWorker) {
+if (window.location.protocol === 'https:' && navigator.serviceWorker) {
     navigator.serviceWorker.register('/service-worker.js').then(() => {
         navigator.serviceWorker.addEventListener('message', e => {
             // service-worker.js 如果更新成功会 postMessage 给页面，内容为 'sw.update'
