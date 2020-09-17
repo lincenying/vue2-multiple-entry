4fe79dfa3f42d1017322dd52fbd5a500a3bb99f1
林岑影 <lincenying@qq.com>
2020-09-17 16:08:59 +0800

    up

--- src/api/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    api: 'https://cnodejs.org/api/v1/',
-    timeout: 30000
-}
--- src/polyfill/index.js
+++
@@ -1,4 +0,0 @@
-import 'core-js/features/promise'
-import 'core-js/features/array/find'
-import 'core-js/features/array/find-index'
-import 'core-js/features/array/includes'
