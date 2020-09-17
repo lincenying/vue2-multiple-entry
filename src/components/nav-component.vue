497957947ab1b7093e01a6c1a276d08f66f69057
林岑影 <lincenying@qq.com>
2020-09-17 16:16:28 +0800

    on 2020-09-17 16:16:28

--- src/components/nav-component.vue
+++ src/components/nav-component.vue
@@ -3,19 +3,19 @@
         <ul class="nav-block">
             <li class="logo"><img src="http://img.daliangju.com/upload/20180603/1528019409246zkdt80.png" alt="" /></li>
             <li :class="{ current: current === 1 }">
-                <a href="../index"><i></i>首页</a>
+                <a href="./index"><i></i>首页</a>
             </li>
             <li :class="{ current: current === 2 }">
-                <a href="../module2"><i></i>模块2</a>
+                <a href="./module2"><i></i>模块2</a>
             </li>
             <li :class="{ current: current === 3 }">
-                <a href="../module3"><i></i>模块3</a>
+                <a href="./module3"><i></i>模块3</a>
             </li>
             <li :class="{ current: current === 4 }">
-                <a href="../router"><i></i>路由</a>
+                <a href="./router"><i></i>路由</a>
             </li>
             <li :class="{ current: current === 5 }">
-                <a href="../vuex"><i></i>Vuex</a>
+                <a href="./vuex"><i></i>Vuex</a>
             </li>
         </ul>
     </div>
