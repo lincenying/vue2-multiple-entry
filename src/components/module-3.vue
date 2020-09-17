166d5c8ba6b201692ad835770621968fbdafe3ec
林岑影 <lincenying@qq.com>
2020-09-17 16:19:24 +0800

    on 2020-09-17 16:19:24

--- src/components/module-3.vue
+++ src/components/module-3.vue
@@ -2,7 +2,7 @@
     <div class="body">
         <div class="header">这是模块 - 3</div>
         <ul class="topics">
-            <li v-for="item in topics" :key="item.id"><a :href="`../view?id=${item.id}`" v-text="item.title" target="_blank"></a></li>
+            <li v-for="item in topics" :key="item.id"><a :href="`./view?id=${item.id}`" v-text="item.title" target="_blank"></a></li>
         </ul>
         <div class="pages">
             <a v-if="page > 1" :href="`?page=${page - 1}`">上一页</a> <a v-else href="javascript:;">上一页</a>
